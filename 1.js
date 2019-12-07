9dea954d94d77bb821a881b2ed9843661870f891
fuba <ecceecce@gmail.com>
2019-12-07 21:04:35 +0900

    Create 1.js

---
+++ 1.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function(nums, target) {
+    var numhash = {};
+    for (var i = 0; i < nums.length; i++) {
+        var num = nums[i];
+        var ok_i = numhash[target-num];
+
+        if (ok_i != undefined) {
+            return [ok_i, i];
+        }
+
+        numhash[num] = i;
+    }
+};
